01ff7c63f91599d95a59b44b5a83640a1bf8e6a2
Vampir <nik.aleks.ru@yandex.ru>
2023-11-04 12:24:00 +0300

    Second commit

---
+++ OOP_SR/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP_SR
+{
+    using ShipGroup = List<Ship>;
+    using WorldMap = List<Port>;
+    struct Ship
+    {
+        public string name;
+        public int weight;
+        public double speed;
+        public double range;
+    }
+
+    struct Port
+    {
+        public string name;
+        public Coordinate coordinates;
+        public List<Ship> ships;
+    }
+
+    struct Coordinate
+    {
+        public double x;
+        public double y;
+
+        public Coordinate(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    struct Route
+    {
+        public List<string> rout;
+        public double length;
+    }
+
+    struct Flight
+    {
+        public string nameFirsPort;
+        public string nameSecondPort;
+        public string nameShip;
+        public Route route;
+        public int time;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
